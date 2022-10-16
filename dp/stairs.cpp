b9ba451ca76edcc64d2a257e160f85b8175d7650
MananMohnot <manan.s.mohnot@gmail.com>
2022-10-16 17:31:21 +0530

    Added DSA codes

---
+++ dp/stairs.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/climbing-stairs/submissions/
+
+
+class Solution {
+public:
+    
+    int dp[46];
+    
+
+    
+    int climbStairs(int n) {
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i = 3; i <=n; i++)
+        {
+        dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+        
+    }
+};
